e3e9d3adf2892623114352baa3c87f05737f1a13
gamboa <ngamboa@insacom.cl>
2022-03-18 22:12:46 -0300

    wip

--- src/test/java/selenium/AppTest.java
+++ src/test/java/selenium/AppTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -66,51 +67,69 @@ public class AppTest {
     }
 
     @Test
-    public void simulacionCompra() {
+    public void shouldGenerarUnaCompra() {
         driver.manage().window().maximize();
         driver.navigate().to("http://automationpractice.com/index.php");
-        new WebDriverWait(driver, 5)
-                .until(ExpectedConditions.elementToBeClickable(By.id("search_query_top")));
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("search_query_top")));
         WebElement searchbox = driver.findElement(By.id("search_query_top"));
         searchbox.sendKeys("blouse");
         searchbox.submit();
         By element = By.cssSelector("a.product_img_link");
-        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(element));
+        wait.until(ExpectedConditions.presenceOfElementLocated(element));
         WebElement itemElement = driver.findElement(element);
         ((JavascriptExecutor) driver).executeScript("javascript:window.scrollBy(250,350)");
-        new WebDriverWait(driver, 5).until(ExpectedConditions.elementToBeClickable(element));
-
+        wait.until(ExpectedConditions.elementToBeClickable(element));
         // mueve mouse sobre prenda
         Actions actionProvider = new Actions(driver);
-        actionProvider.moveToElement( itemElement ).build().perform();
+        actionProvider.moveToElement(itemElement).build().perform();
         // entra a pagina prenda
-        driver.findElement(By.linkText("More")).click();        
+        driver.findElement(By.linkText("More")).click();
 
         // agregamos a la carta
         By button = By.cssSelector("#add_to_cart button");
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(button));
+        wait.until(ExpectedConditions.elementToBeClickable(button));
         WebElement buttonElement = driver.findElement(button);
         buttonElement.click();
 
         By proceed = By.linkText("Proceed to checkout");
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(proceed));
+        wait.until(ExpectedConditions.elementToBeClickable(proceed));
         WebElement proceedElement = driver.findElement(proceed);
         proceedElement.click();
-        
+
         assertEquals("$27.00", driver.findElement(By.id("total_product")).getText());
         assertEquals("$2.00", driver.findElement(By.id("total_shipping")).getText());
         assertEquals("$29.00", driver.findElement(By.id("total_price")).getText());
 
         driver.findElement(By.cssSelector("body")).sendKeys(Keys.PAGE_DOWN);
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(proceed));
+        wait.until(ExpectedConditions.elementToBeClickable(proceed));
         WebElement procToCheckout = driver.findElement(proceed);
         procToCheckout.click();
 
+        String emailToCreate = "YcdUC4rGbuarhGsy@examle.com";
+        By inputEmail = By.id("email_create");
+        By buttonCreateAccount = By.id("SubmitCreate");
+        wait.until(ExpectedConditions.elementToBeClickable(inputEmail));
+        driver.findElement(inputEmail).sendKeys(emailToCreate);
+        driver.findElement(buttonCreateAccount).click();
+
+        By emailFormField = By.id("email");
+        wait.until(ExpectedConditions.elementToBeClickable(emailFormField));
+        assertEquals(emailToCreate, driver.findElement(emailFormField).getText());
+
+        driver.findElement(By.id("id_gender1")).click();
+        driver.findElement(By.id("customer_firstname")).sendKeys("juanito");
+        driver.findElement(By.id("customer_lastname")).sendKeys("perez");
+        driver.findElement(By.id("passwd")).sendKeys("juanitopassword");
+        driver.findElement(By.id("days")).sendKeys(Keys.DOWN, Keys.DOWN);
+        driver.findElement(By.id("months")).sendKeys(Keys.DOWN, Keys.DOWN);
+        driver.findElement(By.id("years")).sendKeys(Keys.DOWN, Keys.DOWN);
+
     }
 
-    @After
-    public void close() {
-        driver.close();
-    }
+    // @After
+    // public void close() {
+    // driver.close();
+    // }
 
 }
