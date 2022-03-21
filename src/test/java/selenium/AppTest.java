106dea20bc65649149d4deccc0102b249bad32e9
gamboa <ngamboa@insacom.cl>
2022-03-21 20:26:22 -0300

    agrega test que genera una compra

--- src/test/java/selenium/AppTest.java
+++ src/test/java/selenium/AppTest.java
@@ -1,18 +1,21 @@
 package selenium;
 
 import static org.junit.Assert.assertEquals;
-import org.junit.After;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.junit.Test;
+import org.junit.After;
 import org.junit.Before;
 
 public class AppTest {
@@ -68,9 +71,9 @@ public class AppTest {
 
     @Test
     public void shouldGenerarUnaCompra() {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
         driver.manage().window().maximize();
         driver.navigate().to("http://automationpractice.com/index.php");
-        WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.elementToBeClickable(By.id("search_query_top")));
         WebElement searchbox = driver.findElement(By.id("search_query_top"));
         searchbox.sendKeys("blouse");
@@ -105,17 +108,18 @@ public class AppTest {
         wait.until(ExpectedConditions.elementToBeClickable(proceed));
         WebElement procToCheckout = driver.findElement(proceed);
         procToCheckout.click();
-
-        String emailToCreate = "YcdUC4rGbuarhGsy@examle.com";
+        String randomEmail = UUID.randomUUID().toString();
+        String emailToCreate = randomEmail + "+YcdUC4rGbuarhGsy@examle.com";
         By inputEmail = By.id("email_create");
         By buttonCreateAccount = By.id("SubmitCreate");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wait.until(ExpectedConditions.elementToBeClickable(inputEmail));
         driver.findElement(inputEmail).sendKeys(emailToCreate);
         driver.findElement(buttonCreateAccount).click();
 
         By emailFormField = By.id("email");
-        wait.until(ExpectedConditions.elementToBeClickable(emailFormField));
-        assertEquals(emailToCreate, driver.findElement(emailFormField).getText());
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("account-creation_form")));
+        assertEquals(emailToCreate, driver.findElement(emailFormField).getAttribute("value"));
 
         driver.findElement(By.id("id_gender1")).click();
         driver.findElement(By.id("customer_firstname")).sendKeys("juanito");
@@ -124,12 +128,58 @@ public class AppTest {
         driver.findElement(By.id("days")).sendKeys(Keys.DOWN, Keys.DOWN);
         driver.findElement(By.id("months")).sendKeys(Keys.DOWN, Keys.DOWN);
         driver.findElement(By.id("years")).sendKeys(Keys.DOWN, Keys.DOWN);
+        driver.findElement(By.id("address1")).sendKeys("avenida falsa 1234");
+        driver.findElement(By.id("city")).sendKeys("valpo");
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.DOWN, Keys.DOWN);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.id("id_state")).sendKeys(Keys.DOWN, Keys.DOWN);
+        driver.findElement(By.id("postcode")).sendKeys("34500");
+        driver.findElement(By.id("phone_mobile")).sendKeys("56912345678");
+        driver.findElement(By.id("submitAccount")).click();
 
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.DOWN, Keys.DOWN, Keys.DOWN, Keys.DOWN, Keys.DOWN);
+        assertEquals("juanito perez",
+                driver.findElement(By.cssSelector(".address_firstname.address_lastname")).getText());
+        assertEquals("avenida falsa 1234",
+                driver.findElement(By.cssSelector(".address_address1.address_address2")).getText());
+        assertEquals("valpo, Alaska 34500",
+                driver.findElement(By.cssSelector(".address_city.address_state_name.address_postcode")).getText());
+        assertEquals("United States", driver.findElement(By.cssSelector(".address_country_name")).getText());
+        assertEquals("56912345678", driver.findElement(By.cssSelector(".address_phone_mobile")).getText());
+
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.DOWN, Keys.DOWN, Keys.DOWN, Keys.DOWN, Keys.DOWN);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("button[name='processAddress']")));
+        driver.findElement(By.cssSelector("button[name='processAddress']")).click();
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("cgv")));
+        driver.findElement(By.id("cgv")).click();
+
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.DOWN, Keys.DOWN, Keys.DOWN, Keys.DOWN, Keys.DOWN);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("button[name='processCarrier']")));
+        driver.findElement(By.cssSelector("button[name='processCarrier']")).click();
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        assertEquals("$27.00", driver.findElement(By.id("total_product")).getText());
+        assertEquals("$2.00", driver.findElement(By.id("total_shipping")).getText());
+        assertEquals("$29.00", driver.findElement(By.id("total_price")).getText());
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.DOWN, Keys.DOWN, Keys.DOWN, Keys.DOWN, Keys.DOWN);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("a[title='Pay by bank wire']")));
+        driver.findElement(By.cssSelector("a[title='Pay by bank wire']")).click();
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        By confirm = By.id("module-bankwire-payment");
+        wait.until(ExpectedConditions.presenceOfElementLocated(confirm));
+        driver.findElement(By.cssSelector("button[type=submit]")).click();
     }
 
-    // @After
-    // public void close() {
-    // driver.close();
-    // }
+    @After
+    public void close() {
+        driver.close();
+    }
 
 }
